1455dc78803bd3f7aba368c21df5981199e79821
michael.grinfeld <michael.grinfeld@dynamicyield.com>
2021-03-04 23:29:42 +0200

    add printout 1st 64 bit

--- src/main/java/com/dy/string2bytearray/Converter.java
+++ src/main/java/com/dy/string2bytearray/Converter.java
@@ -9,11 +9,15 @@ public class Converter {
     public static void main(String[] args) {
         if (args.length == 0)
             throw new IllegalArgumentException("Need at least one parameter");
+
         for (String arg : args) {
             byte[] bytes = Bytes.toBytesBinary(arg);
             byte[] b64 = new byte[8];
             System.arraycopy(bytes, 0, b64, 0, 8);
             System.out.println("1st 8bytes (64bit) as Long -> " + ByteBuffer.wrap(b64).getLong());
+            byte[] bRemain = new byte[bytes.length - 8];
+            System.arraycopy(bytes, 8, bRemain, 0, bytes.length - 8);
+            System.out.println("Last bytes as Long -> " + ByteBuffer.wrap(bRemain).getInt());
             System.out.println(Bytes.toHex(Bytes.toBytesBinary(arg)));
         }
     }
